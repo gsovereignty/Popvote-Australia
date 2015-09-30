5f7497a5fe0b96dc151f8650e1031cbba115b7c9
Gareth Hayes <Gareth.Hayes@gmail.com>
2015-09-30 09:09:16 +0800

    Added author, date, and userID to schema

--- server/schemas.js
+++ server/schemas.js
@@ -5,7 +5,22 @@ Schemas.VotesCollection = new SimpleSchema({
         type: String,
         label: "Title",
         max: 360
+    },
+    userID: {
+        type: String,
+        label: "User ID",
+        optional: true
+    },
+    author: {
+        type: String,
+        label: "Username",
+        max: 20
+    },
+    submitted: {
+        type: Date,
+        label: "Submit Date"
     }
+
 });
 
 VotesCollection.attachSchema(Schemas.VotesCollection);
