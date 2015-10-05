2c6a48f39e618427e8db345fe79a5527b63d5c24
Gareth Hayes <Gareth.Hayes@gmail.com>
2015-10-05 13:22:12 +0800

    Added simply voteYes and voteNo functions

---
+++ client/helpers/voteItem.js
@@ -0,0 +1,12 @@
+Template.voteItem.events({
+    'click .yesvote': function(e) {
+        e.preventDefault();
+        Meteor.call('voteYes', this._id);
+    },
+
+    'click .novote': function(e) {
+        e.preventDefault();
+        Meteor.call('voteNo', this._id);
+    },
+
+});
