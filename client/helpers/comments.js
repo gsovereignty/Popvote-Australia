554be96a7fb0503244576789702281254e30b6dd
Gareth Hayes <Gareth.Hayes@gmail.com>
2015-10-05 13:58:40 +0800

    Added comment 'likes'

--- client/helpers/comments.js
+++ client/helpers/comments.js
@@ -1,4 +1,12 @@
 Template.commentList.helpers({ comments: function() {
-    return Comments.find({voteId: this._id}, {sort: {submitted: -1}});
+    return Comments.find({voteId: this._id}, {sort: {commentLikes: -1}});
     }
+});
+
+Template.commentItem.events({
+    'click .like': function(e) {
+        e.preventDefault();
+        Meteor.call('likeComment', this._id);
+    }
+
 });
