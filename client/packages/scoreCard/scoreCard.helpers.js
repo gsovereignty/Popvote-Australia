ed879ca7f005ab76f1be9f9776149600f9911cb2
Gareth Hayes <Gareth.Hayes@gmail.com>
2015-10-23 12:56:44 +0800

    Experimenting with Politicians layout

--- client/packages/scoreCard/scoreCard.helpers.js
+++ client/packages/scoreCard/scoreCard.helpers.js
@@ -1,6 +1,10 @@
 Template.scoreCard.helpers ({
     politicians: function () {
         return Politicians.find({}, {});
+    },
+
+    indicated: function () {
+        return Politicians.findOne({_id: this._id}, {});
     }
 
 });
