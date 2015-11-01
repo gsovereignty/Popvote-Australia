1e762ea36269c86f8bbcc5be750860bc1f52e834
Gareth Hayes <Gareth.Hayes@gmail.com>
2015-11-01 11:23:53 +1100

    Add sign up form to vote registration

--- client/packages/signup/signup.js
+++ client/packages/signup/signup.js
@@ -141,3 +141,13 @@ Template.signup.helpers({
         return Session.get(block);
     }
 });
+
+
+Template.register.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var emailVar = event.target.registerEmail.value;
+        var passwordVar = event.target.registerPassword.value;
+        console.log("Form submitted.");
+    }
+});
