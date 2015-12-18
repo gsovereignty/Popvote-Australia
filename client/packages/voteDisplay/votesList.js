eaeaee1d4f885f69144cebd1bd29baf2076a38cc
Gareth Hayes <Gareth.Hayes@gmail.com>
2015-12-18 22:45:22 +0800

    Add filter system to votes

--- client/packages/voteDisplay/votesList.js
+++ client/packages/voteDisplay/votesList.js
@@ -1,5 +1,30 @@
+var sortVotes = new ReactiveVar({});
+var voteList = new ReactiveVar({});
+Template.votesList.rendered = function () {
+    if(!sortVotes.get()["totalVotes"]&&!sortVotes.get()["submitted"]) {
+        sortVotes.set({ totalVotes: -1 })
+    }
+};
+
 Template.votesList.helpers({
 	vote: function() {
-return VotesCollection.find({}, {sort: {totalVotes: -1}});
-		}
+return voteList.get()
+		},
+    popularityButton: function(){
+        if(sortVotes.get()["totalVotes"]) {return "btn-active"} else {return "btn-default"}
+    },
+    freshnessButton: function(){
+        if(sortVotes.get()["submitted"]) {return "btn-active"} else {return "btn-default"}
+    }
+});
+
+Template.votesList.events({
+    'click .popularity': function() {sortVotes.set({ totalVotes: -1 })
+    },
+    'click .freshness': function() {sortVotes.set({ submitted: -1 });
+    }
+});
+
+Tracker.autorun(function () {
+    voteList.set(VotesCollection.find({}, {sort: sortVotes.get()}));
 });
